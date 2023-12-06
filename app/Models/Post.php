4b53363551502afd6b165c4c473b230cf72e92bf
MarieCoulay <marie.coulay@gmail.com>
2023-12-06 14:35:51 +0100

    model + relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,14 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
 
-    
+     /**
+     * Les attributs du modèle Post qui définissent les colonnes retrouvées dans la BDD
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'picture',
+    ];
 
+    /**
+     * Relation avec la table User (un post appartient à un user mais un user peut avoir plusieurs posts)
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
    
 }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -42,4 +43,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Relation avec la table Post (un post appartient à un user mais un user peut avoir plusieurs posts)
+     */
+    public function post(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
