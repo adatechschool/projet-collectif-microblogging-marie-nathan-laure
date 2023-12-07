7b538e4120292ae19a7bec11b76f520bad5f9bf3
MarieCoulay <marie.coulay@gmail.com>
2023-12-07 11:18:06 +0100

    ajout view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class PostController extends Controller
 {
     //
+
+    public function index (): View {
+        return view('feed.index');
+    }
 }
